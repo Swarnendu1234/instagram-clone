8a5a1fb99b16576f1703b1628e6857ae24272835
officialpixel01 <officialpixelwavevibes@gmail.com>
2025-07-24 17:38:02 +0530

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST } = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+};
